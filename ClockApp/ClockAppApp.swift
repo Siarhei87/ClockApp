2a8642ef0828ddbf363a9fbd1955cb7910202e2a
Siarhei87 <dolph-1@yandex.ru>
2021-05-05 11:31:35 +0300

    Initial Commit

---
+++ ClockApp/ClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockAppApp.swift
+//  ClockApp
+//
+//  Created by Siarhei Dubko on 5.05.21.
+//
+
+import SwiftUI
+
+@main
+struct ClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
